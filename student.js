15c53c1250cd1abdbea6a58ced6da4fdf1c2939d
phungkhaccuong <cuong.phung@finhay.com.vn>
2020-03-19 15:32:01 +0700

    add student module

---
+++ student.js
@@ -0,0 +1,10 @@
+class student {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    display() {
+        console.log('Name:'+this.name);
+        console.log('Age:'+this.age);
+    }
+}
