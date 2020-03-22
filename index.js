0fb5418aee6743ab39c2b37d4e3679a0cb679a5b
phungkhaccuong <cuong.phung@finhay.com.vn>
2020-03-22 21:37:04 +0700

    change index.js file

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('hello');
+$.ajax({
+    data: someData,
+    dataType: 'json',
+    url: '/path/to/script',
+    success: function(data, textStatus, jqXHR) {
+    // When AJAX call is successfuly
+      console.log('AJAX call successful.');
+      console.log(data);
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+     // When AJAX call has failed
+      console.log('AJAX call failed.');
+      console.log(textStatus + ': ' + errorThrown);
+    },
+    complete: function() {
+    // When AJAX call is complete, will fire upon success or when error is thrown
+      console.log('AJAX call completed');
+    }
+  })
